1f38f9b2c3a14fe21741fffa4433b7a4451cbb56
cubatic45 <cubatic45@gmail.com>
2024-02-03 10:40:30 -0500

    update proxy

--- proxy.go
+++ proxy.go
@@ -10,37 +10,42 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"strings"
+
+	"github.com/tidwall/gjson"
 )
 
 type streamOutput struct {
-	ID                string      `json:"id"`
-	Object            string      `json:"object"`
-	Created           int         `json:"created"`
-	Model             string      `json:"model"`
-	SystemFingerprint interface{} `json:"system_fingerprint"`
+	ID                string `json:"id,omitempty"`
+	Object            string `json:"object"`
+	Created           int    `json:"created,omitempty"`
+	Model             string `json:"model"`
+	SystemFingerprint any    `json:"system_fingerprint,omitempty"`
 	Choices           []struct {
 		Index int `json:"index"`
 		Delta struct {
 			Content string `json:"content"`
+			Role    string `json:"role"`
 		} `json:"delta"`
-		Logprobs     interface{} `json:"logprobs"`
-		FinishReason interface{} `json:"finish_reason"`
+		Logprobs     any `json:"logprobs,omitempty"`
+		FinishReason any `json:"finish_reason"`
 	} `json:"choices"`
 }
 
 // streamOutput inplement json mashal
-func (s streamOutput) MarshalJSON() ([]byte, error) {
-	type Alias streamOutput
-	return json.Marshal(&struct {
-		*Alias
-		Object string `json:"object"`
-	}{
-		Alias:  (*Alias)(&s),
-		Object: "chat.completion.chunk",
-	})
-}
+// func (s streamOutput) MarshalJSON() ([]byte, error) {
+// 	type Alias streamOutput
+// 	return json.Marshal(&struct {
+// 		*Alias
+// 		Object string `json:"object"`
+// 		Model  string `json:"model"`
+// 	}{
+// 		Alias:  (*Alias)(&s),
+// 		Object: "chat.completion.chunk",
+// 	})
+// }
 
 type Proxy struct {
+	model  string
 	stream bool
 	*httputil.ReverseProxy
 }
@@ -50,24 +55,46 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		// handle stream
 		// ...
 		p.ModifyResponse = func(r *http.Response) error {
-			scanStream(r.Body, w)
+			p.scanStream(r.Body, w)
 			return nil
 		}
 	}
 	p.ReverseProxy.ServeHTTP(w, r)
 }
 
-func scanStream(r io.Reader, w io.Writer) {
+func (p *Proxy) scanStream(r io.Reader, w io.Writer) {
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		v := scanner.Bytes()
 		if len(v) == 0 {
 			continue
 		}
+		if bytes.Contains(v, []byte("data: [DONE]")) {
+			w.Write([]byte("data: [DONE]\n"))
+			return
+		}
 		if len(v) > 5 && bytes.Contains(v, []byte("data")) {
 			var output streamOutput
 			if err := json.Unmarshal([]byte(v[5:]), &output); err != nil {
 				fmt.Printf("json unmarshal error: %v\n", err)
+			}
+			if len(output.Choices) == 0 {
+				continue
+			}
+			for i, c := range output.Choices {
+				if c.Delta.Content == "" {
+					continue
+				}
+				if c.Delta.Role == "" {
+					output.Choices[i].Delta.Role = "assistant"
+				}
+			}
+			if output.Model == "" {
+				output.Model = p.model
+			}
+			output.Object = "chat.completion"
+			if p.stream {
+				output.Object = "chat.completion.chunk"
 			}
 			jsonOutput, _ := json.Marshal(output)
 			w.Write([]byte("data: "))
@@ -75,13 +102,26 @@ func scanStream(r io.Reader, w io.Writer) {
 			w.Write([]byte("\n"))
 			continue
 		}
-		w.Write(v)
-		w.Write([]byte("\n"))
 	}
 }
 
 // 所有请求转发到https://api.githubcopilot.com
 func handleProxy(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "post method not allowed")
+		return
+	}
+	p := &Proxy{}
+	bodyBytes, _ := io.ReadAll(r.Body)
+	r.Body.Close() //  must close
+	r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+	bodyJson := gjson.ParseBytes(bodyBytes)
+	if bodyJson.Get("stream").Bool() {
+		p.stream = true
+	}
+	p.model = bodyJson.Get("model").String()
+
 	target, _ := url.Parse("https://api.githubcopilot.com")
 	proxy := httputil.ReverseProxy{
 		Director: func(req *http.Request) {
@@ -102,6 +142,6 @@ func handleProxy(w http.ResponseWriter, r *http.Request) {
 			}
 		},
 	}
-	p := &Proxy{stream: true, ReverseProxy: &proxy}
+	p.ReverseProxy = &proxy
 	p.ServeHTTP(w, r)
 }

bb40512a4f02d6e0da67cc0490f0f65d7e3136be
cubatic45 <cubatic45@gmail.com>
2024-02-05 18:46:01 +0800

    add docker and cli

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"flag"
+	"net/http"
+)
+
+var (
+	copilotToken    string
+	copilotTokenURL string
+)
 
 func main() {
+	copilotToken = *flag.String("token", "", "token to get copilot token")
+	copilotTokenURL = *flag.String("tokenurl", "", "url to get copilot token")
+	flag.Parse()
+	Init()
 	http.HandleFunc("/", handleProxy)
 	http.ListenAndServe(":8081", nil)
 }
